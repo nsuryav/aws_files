715f9a1ae37e8bf630ba5314bef7e94c73129d3e
surya <pilli.suryavardhan116@gmail.com>
2025-09-30 19:13:03 -0400

    Update EC2_instance.tf

--- EC2_instance.tf
+++ EC2_instance.tf
@@ -14,33 +14,3 @@ resource "aws_instance" "my_ec2" {
 
   key_name = "sam"       
 
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
-  
-  egress {
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-
-}
-
-# Get default VPC (needed for security group)
-data "aws_vpc" "default" {
-  default = true
-}
