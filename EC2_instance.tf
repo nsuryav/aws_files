267871e07ac82a56978400884a314c0dddb5ac69
surya <pilli.suryavardhan116@gmail.com>
2025-09-30 19:20:13 -0400

    Update EC2_instance.tf

--- EC2_instance.tf
+++ EC2_instance.tf
@@ -12,7 +12,6 @@ resource "aws_instance" "my_ec2" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro" 
 
-  key_name = "sam"   
   vpc_security_group_ids = [aws_security_group.sann_ssh.id]
 }
 
