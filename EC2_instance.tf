1985a272e9575d8b0d37f7dea2a078b1daa230e3
surya <pilli.suryavardhan116@gmail.com>
2025-09-30 19:15:20 -0400

    Update EC2_instance.tf

--- EC2_instance.tf
+++ EC2_instance.tf
@@ -12,5 +12,35 @@ resource "aws_instance" "my_ec2" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro" 
 
-  key_name = "sam"       
+  key_name = "sam"   
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+}
+
+resource "aws_security_group" "sann_ssh" {
+  name        = "sann_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+}
+
+# Get default VPC (needed for security group)
+data "aws_vpc" "default" {
+  default = true
+}
 
