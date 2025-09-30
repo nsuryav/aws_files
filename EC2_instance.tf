4b72796e5e7098bfcc6f25c6b12e9781259fbbfc
surya <pilli.suryavardhan116@gmail.com>
2025-09-30 19:16:35 -0400

    Update EC2_instance.tf

--- EC2_instance.tf
+++ EC2_instance.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "my_ec2" {
   instance_type = "t3.micro" 
 
   key_name = "sam"   
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.sann_ssh.id]
 }
 
 resource "aws_security_group" "sann_ssh" {
